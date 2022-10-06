fae289d2e048e6395359c47348c04b2777dcc646
J.c - work <Jakob.Chill@aconso.com>
2022-10-06 12:00:28 +0200

    fish: move path to fish.config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,23 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 bind \cs tmux-session-switcher
 bind \es tmux-session-creator
 
+# PATH
 
+set -gx PATH "$HOME/.dotfiles/.bin" $PATH
+set -gx PATH "$HOME/.local/bin" $PATH
+
+## Rust
 set -gx PATH "$HOME/.cargo/bin" $PATH
+
+## Go
 set -gx PATH "/usr/local/go/bin" $PATH
 set -gx PATH "$HOME/go/bin" $PATH
+
+## neovim-bob
+set -gx PATH "$HOME/.local/share/neovim/bin" $PATH
