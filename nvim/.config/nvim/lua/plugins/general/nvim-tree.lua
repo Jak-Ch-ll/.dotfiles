d1a84d92ae870f86ecc4440642c863acaa9ec764
J.c <Jak-Ch-ll@mailbox.org>
2023-02-04 19:33:37 +0100

    refactor(nvim): ♻️ cleanup nvim-tree config

--- nvim/.config/nvim/lua/plugins/general/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/general/nvim-tree.lua
@@ -1,18 +1,20 @@
 return {
     'kyazdani42/nvim-tree.lua',
+
     dependencies = {
         'kyazdani42/nvim-web-devicons',
     },
+
     keys = {
         { '<leader>e', ':NvimTreeToggle<CR>', desc = 'Open [E]xplorer' },
         { '<leader>E', ':NvimTreeFindFile<CR>', desc = 'Open [E]xplorer with current file' },
     },
+
     opts = {
         hijack_cursor = true,
 
-
-        -- files first
         sort_by = function(nodes)
+            -- files first
             table.sort(nodes, function(a, b)
                 -- sort directory after file
                 if (a.type == 'directory' and b.type ~= 'directory') then
@@ -41,7 +43,6 @@ return {
                     col = 100,
                     row = 0,
                     height = 100,
-                    width = 40,
                     border = 'none',
                 },
             }
@@ -85,19 +86,9 @@ return {
         },
 
     },
+
     init = function()
-        --vim.o.splitright = false
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
-
-        -- local augroup = vim.api.nvim_create_augroup("nvim-tree", { clear = true })
-        --
-        -- vim.api.nvim_create_autocmd(
-        --     "BufDelete",
-        --     {
-        --         group = augroup,
-        --         command = 'NvimTreeClose'
-        --     }
-        -- )
     end
 }
