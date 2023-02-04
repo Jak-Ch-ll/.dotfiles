00d03b06882d49d3aef275ddd3bc6ee75f88aaf0
J.c <Jak-Ch-ll@mailbox.org>
2023-02-04 19:21:49 +0100

    feat(nvim): ✨ UPDATE nvim-tree config

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,43 +1,103 @@
 return {
     'kyazdani42/nvim-tree.lua',
-    lazy = false,
     dependencies = {
         'kyazdani42/nvim-web-devicons',
     },
+    keys = {
+        { '<leader>e', ':NvimTreeToggle<CR>', desc = 'Open [E]xplorer' },
+        { '<leader>E', ':NvimTreeFindFile<CR>', desc = 'Open [E]xplorer with current file' },
+    },
     opts = {
-        disable_netrw = true,
-        hijack_unnamed_buffer_when_opening = true,
         hijack_cursor = true,
-        diagnostics = {
-            enable = true
-        },
+
+
+        -- files first
+        sort_by = function(nodes)
+            table.sort(nodes, function(a, b)
+                -- sort directory after file
+                if (a.type == 'directory' and b.type ~= 'directory') then
+                    return false
+                elseif (a.type ~= 'directory' and b.type == 'directory') then
+                    return true
+                end
+
+                return a.name < b.name
+            end)
+        end,
+
         view = {
-            width = '30%',
-            side = 'right',
             number = true,
             relativenumber = true,
+            width = {},
             mappings = {
                 list = {
                     { key = 'l', action = 'preview' },
                     { key = 'h', action = 'close_node' }
                 }
             },
-        }
+            float = {
+                enable = true,
+                open_win_config = {
+                    col = 100,
+                    row = 0,
+                    height = 100,
+                    width = 40,
+                    border = 'none',
+                },
+            }
+        },
+
+        renderer = {
+            add_trailing = true,
+            group_empty = true,
+            indent_width = 4,
+            indent_markers = {
+                enable = true,
+                icons = {
+                    corner = "└", edge = "│", item = "│", bottom = " ", none = " ",
+                }
+            },
+            icons = {
+                git_placement = 'after',
+                modified_placement = 'before',
+            },
+            symlink_destination = false,
+        },
+
+        diagnostics = {
+            enable = true,
+            show_on_dirs = true,
+            show_on_open_dirs = false,
+        },
+
+        git = {
+            show_on_open_dirs = false,
+        },
+
+        modified = {
+            enable = true,
+            show_on_open_dirs = false,
+        },
+
+        live_filter = {
+            prefix = '',
+            always_show_folders = false,
+        },
+
     },
     init = function()
-        vim.o.splitright = false
+        --vim.o.splitright = false
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
 
-        vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
-
-
-        local augroup = vim.api.nvim_create_augroup("nvim-tree", { clear = true })
-
-        vim.api.nvim_create_autocmd(
-            "BufDelete",
-            {
-                group = augroup,
-                command = 'NvimTreeClose'
-            }
-        )
+        -- local augroup = vim.api.nvim_create_augroup("nvim-tree", { clear = true })
+        --
+        -- vim.api.nvim_create_autocmd(
+        --     "BufDelete",
+        --     {
+        --         group = augroup,
+        --         command = 'NvimTreeClose'
+        --     }
+        -- )
     end
 }
