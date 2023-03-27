b958ba82b5c1b9449428f8d751e6bdbcedd3c112
J.c <Jak-Ch-ll@mailbox.org>
2023-03-27 05:31:51 +0200

    refactor (nvim): ♻️ make dap-js use cmd instead of path

--- nvim/.config/nvim/lua/jak-ch-ll/dap/javascript.lua
+++ nvim/.config/nvim/lua/jak-ch-ll/dap/javascript.lua
@@ -1,14 +1,6 @@
 require("dap-vscode-js").setup({
-    -- Path of node executable. Defaults to $NODE_PATH, and then "node"
     node_path = "vite-node",
-
-    -- Path to vscode-js-debug installation
-    debugger_path = "/home/jchill/.local/share/nvim/mason/packages/js-debug-adapter",
-
-    -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-    -- debugger_cmd = { "js-debug-adapter" },
-
-    -- which adapters to register in nvim-dap
+    debugger_cmd = { "js-debug-adapter" },
     adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
 })
 
