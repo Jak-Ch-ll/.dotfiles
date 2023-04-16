c00b7128251598f1d23ee774bf2a5bc806528d13
J.c <Jak-Ch-ll@mailbox.org>
2023-04-16 13:59:49 +0200

    fix(nvim): 🐛 make git fugitive actually load

--- nvim/.config/nvim/lua/plugins/general/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/general/fugitive.lua
@@ -1,6 +1,7 @@
 return {
     'tpope/vim-fugitive',
-    init = function()
-        vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
-    end
+    cmd = 'Git',
+    keys = {
+        { '<leader>gs', ':Git<CR>', desc = "[G]it [S]tatus" },
+    },
 }
