b2483050362a9f1aa40ad17cc2a026014346abdc
J.c <Jak-Ch-ll@mailbox.org>
2023-05-16 19:35:37 +0200

    feat(nvim): ✨ add Treesitter alias

--- nvim/.config/nvim/lua/plugins/general/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/general/treesitter.lua
@@ -1,5 +1,6 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    name = 'Treesitter',
     build = function() require('nvim-treesitter.install').update({ with_sync = true })() end,
     dependencies = {
         'nvim-treesitter/playground',
