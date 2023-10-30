fbe245bb6c475832e2379d28618119d949e644e7
J.c - work <Jakob.Chill@aconso.com>
2023-10-30 15:30:03 +0100

    feat (nvim): ✨ add neogen plugin

---
+++ nvim/.config/nvim/lua/plugins/general/neogen.lua
@@ -0,0 +1,8 @@
+return {
+    "danymat/neogen",
+    event = 'VeryLazy',
+    dependencies = { "nvim-treesitter/nvim-treesitter", 'L3MON4D3/LuaSnip' },
+    config = {
+        snippet_engine = "luasnip"
+    },
+}
