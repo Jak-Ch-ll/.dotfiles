0146d72987567a7b8851d32d24ca560dd0346202
J.c <Jak-Ch-ll@mailbox.org>
2023-12-25 11:20:45 +0100

    nvim: fix commenting stuff

--- nvim/.config/nvim/lua/plugins/general/mini.lua
+++ nvim/.config/nvim/lua/plugins/general/mini.lua
@@ -1,28 +1,35 @@
 return {
-    'echasnovski/mini.nvim',
-    event = 'VeryLazy',
-    lazy = false,
-    version = '*',
-    dependencies = {
-        'JoosepAlviste/nvim-ts-context-commentstring',
-    },
-    config = function()
-        -- require('mini.surround').setup()
-        require('mini.comment').setup({
-            hooks = {
-                pre = function()
-                    require('ts_context_commentstring.internal').update_commentstring({})
-                end,
-            },
-        })
+	'echasnovski/mini.nvim',
+	event = 'VeryLazy',
+	-- lazy = false,
+	-- enabled = false,
+	version = '*',
+	dependencies = {
+		-- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
+		{
+			'JoosepAlviste/nvim-ts-context-commentstring',
+			config = {
+				enable_autocmd = false
+			}
+		}
+	},
+	config = function()
+		-- require('mini.surround').setup()
+		require('mini.comment').setup({
+			options = {
+				custom_commentstring = function()
+					return require('ts_context_commentstring').calculate_commentstring()
+				end,
+			},
+		})
 
-        require('mini.pairs').setup({
-            mappings = {
-                -- Add < and & for Rust lifetimes
-                ["'"] = { neigh_pattern = '[^%a\\&<].' }
-            }
-        })
+		require('mini.pairs').setup({
+			mappings = {
+				-- Add < and & for Rust lifetimes
+				["'"] = { neigh_pattern = '[^%a\\&<].' }
+			}
+		})
 
-        require('mini.ai').setup()
-    end,
+		require('mini.ai').setup()
+	end,
 }
--- nvim/.config/nvim/lua/plugins/general/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/general/treesitter.lua
@@ -1,61 +1,56 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    name = 'Treesitter',
-    build = function() require('nvim-treesitter.install').update({ with_sync = true })() end,
-    dependencies = {
-        -- {
-        --
-        --     'nvim-treesitter/playground',
-        --     cmd = { 'TSPlaygroundToggle', 'TSNodeUnderCursor', 'TSHighlightCapturesUnderCursor' },
-        -- },
-        'nvim-treesitter/nvim-treesitter-context',
-        'windwp/nvim-ts-autotag'
-    },
-    init = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require 'nvim-treesitter.configs'.setup {
-            auto_install = true,
+	'nvim-treesitter/nvim-treesitter',
+	name = 'Treesitter',
+	build = function() require('nvim-treesitter.install').update({ with_sync = true })() end,
+	dependencies = {
+		-- {
+		--
+		--     'nvim-treesitter/playground',
+		--     cmd = { 'TSPlaygroundToggle', 'TSNodeUnderCursor', 'TSHighlightCapturesUnderCursor' },
+		-- },
+		'nvim-treesitter/nvim-treesitter-context',
+		'windwp/nvim-ts-autotag'
+	},
+	init = function()
+		---@diagnostic disable-next-line: missing-fields
+		require 'nvim-treesitter.configs'.setup {
+			auto_install = true,
 
-            highlight = {
-                enable = true,
-            },
+			highlight = {
+				enable = true,
+			},
 
-            indent = {
-                enable = true
-            },
+			indent = {
+				enable = true
+			},
 
-            autotag = {
-                enable = true,
-            },
+			autotag = {
+				enable = true,
+			},
+		}
 
-            context_commentstring = {
-                enable = true,
-                enable_autocmd = false,
-            }
-        }
+		-- io stuff
+		local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+		parser_config.io = {
+			install_info = {
+				url = "~/repos/tree-sitter-io",
+				files = { "src/parser.c" }
+			}
+		}
+		parser_config.prolog = {
+			install_info = {
+				url = 'https://github.com/Rukiza/tree-sitter-prolog',
+				files = { "src/parser.c" },
+			},
+			filetype = "pl"
+		}
 
-        -- io stuff
-        local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-        parser_config.io = {
-            install_info = {
-                url = "~/repos/tree-sitter-io",
-                files = { "src/parser.c" }
-            }
-        }
-        parser_config.prolog = {
-            install_info = {
-                url = 'https://github.com/Rukiza/tree-sitter-prolog',
-                files = { "src/parser.c" },
-            },
-            filetype = "pl"
-        }
-
-        vim.filetype.add({
-            extension = {
-                io = 'io',
-                pl = 'prolog',
-                mdx = 'markdown'
-            }
-        })
-    end,
+		vim.filetype.add({
+			extension = {
+				io = 'io',
+				pl = 'prolog',
+				mdx = 'markdown'
+			}
+		})
+	end,
 }
