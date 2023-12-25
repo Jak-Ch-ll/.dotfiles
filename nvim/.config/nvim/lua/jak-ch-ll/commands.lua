b3ab1b1ff7ce05dbfbdf3cd2d31ad1e329880513
J.c <Jak-Ch-ll@mailbox.org>
2023-12-25 11:17:37 +0100

    nvim: fix missing ts-query error

--- nvim/.config/nvim/lua/jak-ch-ll/commands.lua
+++ nvim/.config/nvim/lua/jak-ch-ll/commands.lua
@@ -1,6 +1,7 @@
-local embedded_vue = vim.treesitter.query.parse(
-    "typescript",
-    [[
+local function get_embedded_vue()
+	return vim.treesitter.query.parse(
+		"typescript",
+		[[
         (variable_declarator
             type: (type_annotation
                 (type_identifier) @type (#eq? @type "Story")
@@ -22,78 +23,80 @@ local embedded_vue = vim.treesitter.query.parse(
             )
         )
         ]]
-)
+	)
+end
 
 local function get_root(bufnr)
-    local parser = vim.treesitter.get_parser(bufnr, "typescript", {})
-    local tree = parser:parse()[1]
-    return tree:root()
+	local parser = vim.treesitter.get_parser(bufnr, "typescript", {})
+	local tree = parser:parse()[1]
+	return tree:root()
 end
 
 local function format_embedded_vue(bufnr)
-    bufnr = bufnr or vim.api.nvim_get_current_buf()
+	bufnr = bufnr or vim.api.nvim_get_current_buf()
 
-    if vim.bo[bufnr].filetype ~= "typescript" then
-        vim.notify("Can only be used in TypeScript files")
-        return
-    end
+	if vim.bo[bufnr].filetype ~= "typescript" then
+		vim.notify("Can only be used in TypeScript files")
+		return
+	end
 
-    local root = get_root(bufnr)
+	local root = get_root(bufnr)
+	local embedded_vue = get_embedded_vue()
 
-    local changes = {}
-    for id, node in embedded_vue:iter_captures(root, bufnr, 0, -1) do
-        -- print("iteration", id, node)
-        local name = embedded_vue.captures[id]
+	local changes = {}
+	for id, node in embedded_vue:iter_captures(root, bufnr, 0, -1) do
+		-- print("iteration", id, node)
+		local name = embedded_vue.captures[id]
 
-        if name == 'vue' then
-            -- { startRow, startCol, endRow, endCol }
-            local range = { node:range() }
+		if name == 'vue' then
+			-- { startRow, startCol, endRow, endCol }
+			local range = { node:range() }
 
-            local unformatted = vim.treesitter.get_node_text(node, bufnr)
-            local result = vim.fn.system('prettierd index.html', unformatted)
+			local unformatted = vim.treesitter.get_node_text(node, bufnr)
+			local result = vim.fn.system('prettierd index.html', unformatted)
 
-            local lines = {}
-            for line in string.gmatch(result, "([^\n]*)\n?") do
-                table.insert(lines, line)
-            end
+			local lines = {}
+			for line in string.gmatch(result, "([^\n]*)\n?") do
+				table.insert(lines, line)
+			end
 
-            -- remove trailing empty line
-            table.remove(lines)
+			-- remove trailing empty line
+			table.remove(lines)
 
-            local indentation = string.rep(" ", 6)
-            for idx, line in ipairs(lines) do
-                if idx == table.maxn(lines) then
-                    lines[idx] = '    ' .. line
-                elseif idx ~= 1 then
-                    lines[idx] = indentation .. line
-                end
-            end
+			local indentation = string.rep(" ", 6)
+			for idx, line in ipairs(lines) do
+				if idx == table.maxn(lines) then
+					lines[idx] = '    ' .. line
+				elseif idx ~= 1 then
+					lines[idx] = indentation .. line
+				end
+			end
 
-            table.insert(changes, 1, {
-                start_row = range[1],
-                start_col = range[2],
-                end_row = range[3],
-                end_col = range[4],
-                lines = lines
-            })
-        end
-    end
+			table.insert(changes, 1, {
+				start_row = range[1],
+				start_col = range[2],
+				end_row = range[3],
+				end_col = range[4],
+				lines = lines
+			})
+		end
+	end
 
-    for _, change in ipairs(changes) do
-        vim.api.nvim_buf_set_text(
-            bufnr,
-            change.start_row,
-            change.start_col,
-            change.end_row,
-            change.end_col,
-            change.lines
-        )
-    end
+	for _, change in ipairs(changes) do
+		vim.api.nvim_buf_set_text(
+			bufnr,
+			change.start_row,
+			change.start_col,
+			change.end_row,
+			change.end_col,
+			change.lines
+		)
+	end
 end
 
 
 vim.api.nvim_create_user_command("VueStorybookFormat", function()
-    format_embedded_vue()
+	format_embedded_vue()
 end, {})
 
 -- vim.api.nvim_create_autocmd("BufWritePre", {
