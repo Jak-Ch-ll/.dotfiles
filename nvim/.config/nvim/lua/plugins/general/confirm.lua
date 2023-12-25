de566c69744d9b670b8051ca6610b605095a4cca
J.c <Jak-Ch-ll@mailbox.org>
2023-12-25 11:21:22 +0100

    nvim: add prettierd and nixfmt

--- nvim/.config/nvim/lua/plugins/general/confirm.lua
+++ nvim/.config/nvim/lua/plugins/general/confirm.lua
@@ -1,31 +1,31 @@
-local prettier = { "prettier" }
+local prettier = { { "prettierd", "prettier" } }
 
 return {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    opts = {
-        formatters_by_ft = {
-            javascript = prettier,
-            typescript = prettier,
-            typescriptreact = prettier,
-            vue = prettier,
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			javascript = prettier,
+			typescript = prettier,
+			typescriptreact = prettier,
+			vue = prettier,
 
-            html = prettier,
-            css = prettier,
-            scss = prettier,
+			html = prettier,
+			css = prettier,
+			scss = prettier,
 
-            xml = prettier,
-            yaml = prettier,
-            json = prettier,
-            jsonc = prettier,
-            markdown = prettier,
+			xml = prettier,
+			yaml = prettier,
+			json = prettier,
+			jsonc = prettier,
+			markdown = prettier,
 
-            nix = { 'alejandra' }
-        },
-        format_on_save = {
-            lsp_fallback = true,
-            timeout_ms = 500,
-        },
-    }
+			nix = { 'nixfmt' }
+		},
+		format_on_save = {
+			lsp_fallback = true,
+			timeout_ms = 500,
+		},
+	}
 }
