11596015649b00c8e605618471cf75a3d7375370
J.c <Jak-Ch-ll@mailbox.org>
2024-03-29 13:45:47 +0100

    nvim: add typescript-tools (disabled)

--- nvim/.config/nvim/lua/plugins/lsp/languages/typescript.lua
+++ nvim/.config/nvim/lua/plugins/lsp/languages/typescript.lua
@@ -1,3 +1,37 @@
 return {
-    'jose-elias-alvarez/typescript.nvim',
+	'pmizio/typescript-tools.nvim',
+	dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+	enabled = false,
+	ft = { 'typescript', 'typescriptreact', 'vue' },
+	config = function()
+		local on_attach = require('utils/lsp/on-attach')
+		local mason_registry = require('mason-registry')
+		local vue_language_server_path = mason_registry
+			.get_package('vue-language-server')
+			:get_install_path() .. '/node_modules/@vue/language-server'
+
+		require('typescript-tools').setup({
+			on_attach = on_attach,
+			filetypes = { 'typescript', 'typescriptreact', 'vue' },
+			settings = {
+				tsserver_plugins = {
+					{
+						name = '@vue/typescript-plugin',
+						location = vue_language_server_path,
+						languages = { 'vue' },
+					},
+				},
+				tsserver_file_preferences = {
+					includeInlayParameterNameHints = 'all',
+					includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+					includeInlayFunctionParameterTypeHints = true,
+					includeInlayVariableTypeHints = true,
+					includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+					includeInlayPropertyDeclarationTypeHints = true,
+					includeInlayFunctionLikeReturnTypeHints = true,
+					includeInlayEnumMemberValueHints = true,
+				},
+			},
+		})
+	end,
 }
