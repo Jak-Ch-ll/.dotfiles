e97c4ab2fc2346b28fb5b5fae79595acf7ea4f0c
Jak-Ch-ll <jak-ch-ll@mailbox.org>
2024-03-29 19:10:35 +0100

    tmux: show current jj revision

--- .nix/apps/tmux.nix
+++ .nix/apps/tmux.nix
@@ -1,16 +1,15 @@
-{pkgs, ...}: let
-  tpm =
-    pkgs.tmuxPlugins.mkTmuxPlugin
-    {
-      pluginName = "tpm";
-      version = "unstable-2023-11-13";
-      src = pkgs.fetchFromGitHub {
-        owner = "tmux-plugins";
-        repo = "tpm";
-        rev = "99469c4a9b1ccf77fade25842dc7bafbc8ce9946";
-        sha256 = "sha256-hW8mfwB8F9ZkTQ72WQp/1fy8KL1IIYMZBtZYIwZdMQc=";
-      };
+{ pkgs, ... }:
+let
+  tpm = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "tpm";
+    version = "unstable-2023-11-13";
+    src = pkgs.fetchFromGitHub {
+      owner = "tmux-plugins";
+      repo = "tpm";
+      rev = "99469c4a9b1ccf77fade25842dc7bafbc8ce9946";
+      sha256 = "sha256-hW8mfwB8F9ZkTQ72WQp/1fy8KL1IIYMZBtZYIwZdMQc=";
     };
+  };
 in {
   programs.tmux = {
     enable = true;
@@ -38,18 +37,16 @@ in {
 
     '';
 
-    plugins = [
-      {
-        plugin = tpm;
-        extraConfig = ''
-          set -g @plugin 'jimeh/tmux-themepack'
-          set -g @themepack 'powerline/default/purple'
-          run-shell '${tpm}/share/tmux-plugins/tpm/tpm'
+    plugins = [{
+      plugin = tpm;
+      extraConfig = ''
+        set -g @plugin 'jimeh/tmux-themepack'
+        set -g @themepack 'powerline/default/purple'
+        run-shell '${tpm}/share/tmux-plugins/tpm/tpm'
 
-          # show git branch in status bar
-          set -g status-right '#(git -C #{pane_current_path} rev-parse --abbrev-ref HEAD 2> /dev/null) '
-        '';
-      }
-    ];
+        # show git branch in status bar
+        set -g status-right '[#(jj-git-rev #{pane_current_path})]'
+      '';
+    }];
   };
 }
