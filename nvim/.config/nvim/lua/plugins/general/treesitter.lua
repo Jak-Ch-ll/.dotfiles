d9f4c28f7c70eaca1766a51b402bbcc06da3285e
Jak-Ch-ll <jak-ch-ll@mailbox.org>
2024-04-02 17:00:26 +0200

    nvim: fix treesitter parser issue (hopefully)

--- nvim/.config/nvim/lua/plugins/general/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/general/treesitter.lua
@@ -1,20 +1,40 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
 	name = 'Treesitter',
-	build = function() require('nvim-treesitter.install').update({ with_sync = true })() end,
+	build = function()
+		require('nvim-treesitter.install').update({ with_sync = true })()
+	end,
 	dependencies = {
 		-- {
-		--
 		--     'nvim-treesitter/playground',
 		--     cmd = { 'TSPlaygroundToggle', 'TSNodeUnderCursor', 'TSHighlightCapturesUnderCursor' },
 		-- },
 		'nvim-treesitter/nvim-treesitter-context',
-		'windwp/nvim-ts-autotag'
+		'windwp/nvim-ts-autotag',
 	},
 	init = function()
 		---@diagnostic disable-next-line: missing-fields
-		require 'nvim-treesitter.configs'.setup {
-			ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'css', 'vue' },
+		require('nvim-treesitter.configs').setup({
+			ensure_installed = {
+				-- come pre-installed with Neovim and might create issues if not
+				-- installed with Treesitter as well
+				'bash',
+				'c',
+				'lua',
+				'markdown',
+				'python',
+				'vim',
+				'query',
+				'vimdoc',
+
+				'go',
+				'rust',
+
+				'javascript',
+				'typescript',
+				'css',
+				'vue',
+			},
 
 			auto_install = true,
 
@@ -23,36 +43,37 @@ return {
 			},
 
 			indent = {
-				enable = true
+				enable = true,
 			},
 
 			autotag = {
 				enable = true,
 			},
-		}
+		})
 
 		-- io stuff
-		local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+		local parser_config =
+			require('nvim-treesitter.parsers').get_parser_configs()
 		parser_config.io = {
 			install_info = {
-				url = "~/repos/tree-sitter-io",
-				files = { "src/parser.c" }
-			}
+				url = '~/repos/tree-sitter-io',
+				files = { 'src/parser.c' },
+			},
 		}
 		parser_config.prolog = {
 			install_info = {
 				url = 'https://github.com/Rukiza/tree-sitter-prolog',
-				files = { "src/parser.c" },
+				files = { 'src/parser.c' },
 			},
-			filetype = "pl"
+			filetype = 'pl',
 		}
 
 		vim.filetype.add({
 			extension = {
 				io = 'io',
 				pl = 'prolog',
-				mdx = 'markdown'
-			}
+				mdx = 'markdown',
+			},
 		})
 	end,
 }
