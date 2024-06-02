77f46c75729fd139dacf937e913f5b251f2bed71
J.c <Jak-Ch-ll@mailbox.org>
2024-06-02 15:45:36 +0200

    feat(nix): 🔥 REMOVE nvim-nightly overlay

--- .nix/flake.nix
+++ .nix/flake.nix
@@ -8,21 +8,19 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      overlays = [ inputs.neovim-nightly-overlay.overlay ];
     in {
       homeConfigurations."j" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix { nixpkgs.overlays = overlays; } ];
+        modules = [ ./home.nix ];
       };
     };
 }
