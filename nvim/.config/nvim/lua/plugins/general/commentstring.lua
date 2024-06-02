d1083c0b84f2103d869fb6fb40a4db0c16ef9824
Jak-Ch-ll <jak-ch-ll@mailbox.org>
2024-06-02 15:54:22 +0200

    nvim: remove mini.comment

---
+++ nvim/.config/nvim/lua/plugins/general/commentstring.lua
@@ -0,0 +1,26 @@
+-- This plugin might become redundant in the future, because of the new built-in
+-- commenting plugin that automatially uses Treesitter
+--
+-- Currently it solves 2 issues:
+-- 1. It works with the template part of Vue files; currently the parses does
+--    not seem do expose a commentstring
+-- 2. It adds a space before the comments, which honestly is not super important
+--    for commented out code and has a potential workaround:
+--    https://github.com/neovim/neovim/pull/28176#issuecomment-2051944146
+return {
+	'JoosepAlviste/nvim-ts-context-commentstring',
+	config = {
+		enable_autocmd = false,
+	},
+	init = function()
+		-- Integrate  with built-it commenting plugin
+		-- Keep an eye on https://github.com/JoosepAlviste/nvim-ts-context-commentstring/issues/109
+		local get_option = vim.filetype.get_option
+		---@diagnostic disable-next-line: duplicate-set-field
+		vim.filetype.get_option = function(filetype, option)
+			return option == 'commentstring'
+					and require('ts_context_commentstring.internal').calculate_commentstring()
+				or get_option(filetype, option)
+		end
+	end,
+}
--- nvim/.config/nvim/lua/plugins/general/mini.lua
+++ nvim/.config/nvim/lua/plugins/general/mini.lua
@@ -4,30 +4,12 @@ return {
 	-- lazy = false,
 	-- enabled = false,
 	version = '*',
-	dependencies = {
-		-- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
-		{
-			'JoosepAlviste/nvim-ts-context-commentstring',
-			config = {
-				enable_autocmd = false
-			}
-		}
-	},
 	config = function()
-		-- require('mini.surround').setup()
-		require('mini.comment').setup({
-			options = {
-				custom_commentstring = function()
-					return require('ts_context_commentstring').calculate_commentstring()
-				end,
-			},
-		})
-
 		require('mini.pairs').setup({
 			mappings = {
 				-- Add < and & for Rust lifetimes
-				["'"] = { neigh_pattern = '[^%a\\&<].' }
-			}
+				["'"] = { neigh_pattern = '[^%a\\&<].' },
+			},
 		})
 
 		require('mini.ai').setup()
