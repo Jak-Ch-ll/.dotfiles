a0e8f258ea0a1f4a790da58626c3acb81c256d40
Jak-Ch-ll <jak-ch-ll@mailbox.org>
2025-05-11 20:26:58 +0200

    nvim: add lazydev plugin

--- nvim/.config/nvim/after/lsp/lua_ls.lua
+++ nvim/.config/nvim/after/lsp/lua_ls.lua
@@ -1,32 +1,5 @@
 ---@type vim.lsp.Config
 return {
-	on_init = function(client)
-		local path = client.workspace_folders[1].name
-		if
-			vim.loop.fs_stat(path .. '/.luarc.json')
-			or vim.loop.fs_stat(path .. '/.luarc.jsonc')
-		then
-			return
-		end
-
-		client.config.settings.Lua =
-			---@diagnostic disable-next-line: param-type-mismatch
-			vim.tbl_deep_extend('force', client.config.settings.Lua, {
-				runtime = {
-					version = 'LuaJIT',
-				},
-				-- Make the server aware of Neovim runtime files
-				workspace = {
-					checkThirdParty = false,
-					library = {
-						vim.env.VIMRUNTIME,
-					},
-				},
-				diagnostics = {
-					disable = { 'undefined-field' },
-				},
-			})
-	end,
 	settings = {
 		Lua = {
 			-- Do not send telemetry data containing a randomized but unique identifier
