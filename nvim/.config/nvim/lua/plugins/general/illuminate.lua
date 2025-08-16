96879bad3cd41ea3596f57a4d84d16b5addc0fbc
Jak-Ch-ll <jak-ch-ll@mailbox.org>
2025-08-16 18:23:01 +0200

    nvim: fix illuminate issues

--- nvim/.config/nvim/lua/plugins/general/illuminate.lua
+++ nvim/.config/nvim/lua/plugins/general/illuminate.lua
@@ -1,6 +1,15 @@
 -- https://github.com/RRethy/vim-illuminate
 
 return {
-    'RRethy/vim-illuminate',
-    event = 'VeryLazy',
+	'RRethy/vim-illuminate',
+	event = 'VeryLazy',
+	config = function()
+		require('illuminate').configure({
+			delay = 500,
+			filetypes_denylist = {
+				'NvimTree',
+			},
+			modes_denylist = { 'i' },
+		})
+	end,
 }
