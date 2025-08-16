6209653714e2ebc55842135ff926ac3d21360f8f
Jak-Ch-ll <jak-ch-ll@mailbox.org>
2025-08-16 18:09:08 +0200

    neovim: disable luarocks for lazy.nvim

--- nvim/.config/nvim/lua/jak-ch-ll/lazy.lua
+++ nvim/.config/nvim/lua/jak-ch-ll/lazy.lua
@@ -1,6 +1,6 @@
 -- bootstrap lazy
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
 	vim.fn.system({
 		'git',
 		'clone',
@@ -20,4 +20,7 @@ require('lazy').setup('plugins', {
 		path = '~/projects',
 	},
 	lockfile = vim.fn.stdpath('config') .. '/lua/plugins/lazy-lock.json',
+	rocks = {
+		enabled = false,
+	},
 })
