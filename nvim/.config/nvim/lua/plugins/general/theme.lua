ea18ba0f4bd785a07d4f8758c6377e0925dcf492
jak-ch-ll <jak-ch-ll@mailbox.org>
2026-03-23 09:12:21 +0100

    nvim: replace undercurl with underline

--- nvim/.config/nvim/lua/plugins/general/theme.lua
+++ nvim/.config/nvim/lua/plugins/general/theme.lua
@@ -2,15 +2,20 @@ function ColorMyPencils(color)
 	color = color or 'tokyonight'
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+	vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 end
 
+---@module 'lazy'
+---@type LazySpec
 return {
 	{
 		'folke/tokyonight.nvim',
 		lazy = false,
 		priority = 1000,
+
+		---@module 'tokyonight'
+		---@type tokyonight.Config
 		opts = {
 			style = 'night',
 			transparent = true,
@@ -18,37 +23,45 @@ return {
 				sidebars = 'transparent',
 				floats = 'transparent',
 				comments = { italic = true },
-				keywords = { italic = false }
-			}
+				keywords = { italic = false },
+			},
+			on_highlights = function(highlights, _colors)
+				-- Replace undercurl with underline
+				-- https://github.com/folke/tokyonight.nvim/discussions/303#discussioncomment-4808330
+				highlights.DiagnosticUnderlineWarn.undercurl = nil
+				highlights.DiagnosticUnderlineError.undercurl = nil
+				highlights.DiagnosticUnderlineWarn.underline = true
+				highlights.DiagnosticUnderlineError.underline = true
+			end,
 		},
 		init = function()
 			-- vim.g.tokyonight_transparent_sidebar = true
 			vim.cmd([[colorscheme tokyonight]])
-		end
+		end,
 	},
 
 	{
 		'ayu-theme/ayu-vim',
 		config = function()
 			vim.g.ayucolor = 'dark' -- alt: mirage
-		end
+		end,
 	},
 
 	{
 		'olimorris/onedarkpro.nvim',
 		config = function()
-			local onedarkpro = require("onedarkpro")
-			require("onedarkpro").setup({
+			local onedarkpro = require('onedarkpro')
+			require('onedarkpro').setup({
 				styles = {
-					comments = "italic",
-					keywords = "bold"
+					comments = 'italic',
+					keywords = 'bold',
 				},
 				options = {
 					undercurl = true,
-					transparency = true
-				}
+					transparency = true,
+				},
 			})
 			onedarkpro.load()
-		end
-	}
+		end,
+	},
 }
