6babf9624918711b065b1a4d7f3e25649157bc3c
Jak-Ch-ll <jak-ch-ll@mailbox.org>
2026-03-28 16:52:08 +0100

    neovim: customize render-markdown

--- nvim/.config/nvim/lua/plugins/general/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/general/render-markdown.lua
@@ -4,11 +4,26 @@ return {
 		'nvim-treesitter/nvim-treesitter',
 		'nvim-tree/nvim-web-devicons',
 	},
+
 	event = 'VeryLazy',
 
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
 	opts = {
 		completions = { lsp = { enabled = true } },
+
+		render_modes = { 'n', 'c', 't', 'i', 'v' },
+
+		heading = {
+			width = 'block',
+		},
+
+		code = {
+			conceal_delimiters = false,
+			border = 'thin',
+			left_pad = 1,
+			right_pad = 1,
+			width = 'block',
+		},
 	},
 }
